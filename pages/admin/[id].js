ec2b20f31ddb4d430acb018d2844dafb1adf83b2
Jhon Rey <jhonreymendiola@gmail.com>
2021-07-22 12:27:26 +0800

    re

--- pages/admin/[id].js
+++ pages/admin/[id].js
@@ -1,3 +1,12 @@
+const Details = () => {
+    return ( 
+        <div>
+            <h2>details</h2>
+        </div>
+     );
+}
+ 
+export default Details;
 // import { useHistory } from "react-router-dom"
 
 // import { useRouter } from 'next/router'
---
+++ pages/bloglist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function bloglist() {
+    return (
+        <div>
+            <h1>bloglist</h1>
+        </div>
+    )
+}
+
+export default bloglist
